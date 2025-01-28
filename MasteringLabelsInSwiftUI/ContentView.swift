d784f2f0d3ef576bf5b08801effe2dc935e27e28
ribragimov-mwb <ribragimov@malwarebytes.com>
2025-01-28 09:35:37 +0200

    init

--- MasteringLabelsInSwiftUI/ContentView.swift
+++ MasteringLabelsInSwiftUI/ContentView.swift
@@ -9,16 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Label {
+            Text("Mastering Labels in SwiftUI")
+        } icon: {
+            Image(systemName: "swift")
         }
-        .padding()
+        .labelStyle(CustomLabelStyle(iconColor: .mint, titleColor: .orange, backgroundColor: .mint))
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CustomLabelStyle: LabelStyle {
+    let iconColor: Color
+    let titleColor: Color
+    let backgroundColor: Color
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack(alignment: .firstTextBaseline) {
+            configuration.icon
+                .foregroundStyle(iconColor.gradient)
+            configuration.title
+                .foregroundStyle(titleColor.gradient)
+            configuration.icon
+                .foregroundStyle(iconColor.gradient)
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 15)
+                .fill(backgroundColor.gradient.opacity(0.2)))
+    }
 }
