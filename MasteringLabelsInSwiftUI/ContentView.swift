c2e1ff51cb37bc2c09409f95b772065ec70b63fb
ribragimov-mwb <ribragimov@malwarebytes.com>
2025-01-28 09:25:44 +0200

    Initial Commit

---
+++ MasteringLabelsInSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MasteringLabelsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 1/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MasteringLabelsInSwiftUI/MasteringLabelsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringLabelsInSwiftUIApp.swift
+//  MasteringLabelsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MasteringLabelsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
